fdc9bd45723da5da9d34bcf6d510fb5183d9a959
ZhengkaiXu <695378091@qq.com>
2024-02-03 03:08:32 +0800

    20240203

---
+++ 程序清单/第11章/repeat.c/main.c
@@ -0,0 +1,12 @@
+/* repeat.c -- 带参数的main() */
+#include <stdio.h>
+// repeat Resistance is futile
+int main(int argc, char* argv[]) {
+    int count;
+
+    printf("The command line has %d arguments:\n", argc - 1);
+    for (count = 1; count < argc; count++)
+        printf("%d: %s\n", count, argv[count]);
+    printf("\n");
+    return 0;
+}
