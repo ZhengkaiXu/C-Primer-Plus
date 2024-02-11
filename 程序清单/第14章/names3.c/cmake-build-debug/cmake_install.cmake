b93177e58515f1e2692b1e31141e6ddb9af4f12b
ZhengkaiXu <695378091@qq.com>
2024-02-12 02:25:40 +0800

    20240212

--- 程序清单/第14章/names3.c/cmake-build-debug/cmake_install.cmake
+++ 程序清单/第14章/names3.c/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/xuzhengkai/Desktop/study/C Primer Plus/程序清单/第14章/names3.c
+# Install script for directory: /Users/xuzhengkai/Desktop/study/C语言/C Primer Plus/程序清单/第14章/names3.c
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/xuzhengkai/Desktop/study/C Primer Plus/程序清单/第14章/names3.c/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/xuzhengkai/Desktop/study/C语言/C Primer Plus/程序清单/第14章/names3.c/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
