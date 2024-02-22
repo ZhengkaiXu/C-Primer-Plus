5b3d0c05d10bb9be33c5e8fecdae05443d0eec36
ZhengkaiXu <695378091@qq.com>
2024-02-23 02:22:34 +0800

    20240223

---
+++ 程序清单/第16章/doubinc1.c/names.h
@@ -0,0 +1,27 @@
+//
+// Created by 徐钲开 on 2024/2/22.
+//
+// names.h -- 修订后的 names_st 头文件，避免重复包含
+
+#ifndef NAMES_H_
+#define NAMES_H_
+
+// 明示常量
+#define SLEN 32
+
+// 结构声明
+struct name_st
+{
+    char first[SLEN];
+    char last[SLEN];
+};
+
+// 类型定义
+typedef struct name_st names;
+
+// 函数原型
+void get_names(names*);
+void show_names(const names*);
+char* s_gets(char* st, int n);
+
+#endif
